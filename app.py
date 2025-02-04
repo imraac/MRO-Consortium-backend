4a1732c6b30f6b5fecb4674bb038f92496f9d8bf
imraac <agmraan@gmail.com>
2025-02-04 23:19:42 +0300

    registration

--- app.py
+++ app.py
@@ -146,7 +146,8 @@ class Login(Resource):
         user = User.query.filter_by(email=email).first()
 
         if user and bcrypt.check_password_hash(user.password, password):
-            access_token = create_access_token(identity=user.id)
+            access_token = create_access_token(identity=str(user.id))
+
             session['user_id'] = user.id 
 
             login_entry = LoginHistory(user_id=user.id, login_time=datetime.utcnow())
